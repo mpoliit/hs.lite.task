ef6f65679d125637f8f76eeabee841300bf2ed41
m.poliit <maks.poliit@gmail.com>
2020-03-23 18:47:35 +0200

    add view result

--- index.php
+++ index.php
@@ -3,4 +3,7 @@ require_once 'Worker.php';
 
 $jack = new Worker('Jack', 25, 1000);
 
-var_dump($jack->getName());
+if (!is_null($jack->getAge()) && !is_null($jack->getSalary())){
+    echo $jack->getAge() . ' * ' . $jack->getSalary() . ' = ' . $jack->getAge() * $jack->getSalary();
+
+};

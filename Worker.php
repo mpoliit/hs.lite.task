1144251ef26906a1f52e70b6d9dd44f8bc6a814a
m.poliit <maks.poliit@gmail.com>
2020-03-23 19:06:02 +0200

    hot fix

--- Worker.php
+++ Worker.php
@@ -11,8 +11,8 @@ class Worker
     {
         if ($age>17 AND $age<60) {
             $this->name = $name;
-            $this->age = $age;
-            $this->salary = $salary;
+            $this->age = (int)$age;
+            $this->salary = (float)$salary;
         } else {
             print_r('Возраст сотрудника может быть с 18 до 60<br>');
         }

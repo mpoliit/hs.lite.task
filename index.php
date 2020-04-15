49ee5ee81b7907ccdc3beaafc4dd0994d8a2eefc
m.poliit <maks.poliit@gmail.com>
2020-04-15 17:21:14 +0300

    add task13

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+class Product {
+    public function get($name) {
+
+    }
+
+    public function set($name, $value) {
+
+    }
+
+    public function save() {
+
+    }
+
+    public function update() {
+
+    }
+
+    public function delete() {
+
+    }
+}
+
+class ViewProduct {
+    public function show(Product $product) {
+
+    }
+
+    public function print(Product $product) {
+
+    }
+}
+
